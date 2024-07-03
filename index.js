74fa1e334f08352a82ee6cf2189dda7f41748dbf
pixelboogie <41452340+pixelboogie@users.noreply.github.com>
2024-07-03 14:49:35 -0500

    06

--- index.js
+++ index.js
@@ -3,12 +3,28 @@ import { openai } from './config.js';
 const asstID = "asst_wUu8fCNZ8zvS2UB5OUP4M2ac";
 const threadID = "thread_8sc0gOJzhxWG2b5pPRSwGuLt";
 
+// Get the current run
+// const currentRun = await openai.beta.threads.runs.retrieve(
+//   threadID,
+//   "run_8Y9joh5ACv06TBVXwtYE32Hq"
+// );
+// console.log("Run status: " + currentRun.status);
+
+// Run the assistant's thread
+async function runThread(){
+    const run = await openai.beta.threads.runs.create(
+    threadID,
+    { assistant_id: asstID }
+  );
+  console.log(run);
+}
+runThread()
+
 // Create a message for the thread
-async function createMessage(){
-    const threadMessages = await openai.beta.threads.messages.create(
+async function createMessage() {
+  const threadMessages = await openai.beta.threads.messages.create(
     threadID,
     { role: "user", content: "Can you recommend a comedy?" }
   );
   console.log(threadMessages);
 }
-createMessage();
