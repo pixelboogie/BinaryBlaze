3882daf45c4d949a505ba77194c8726945be5fe8
pixelboogie <41452340+pixelboogie@users.noreply.github.com>
2024-07-03 14:47:42 -0500

    05

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
 import { openai } from './config.js';
 
 const asstID = "asst_wUu8fCNZ8zvS2UB5OUP4M2ac";
+const threadID = "thread_8sc0gOJzhxWG2b5pPRSwGuLt";
 
-// Create Movie expert assistant
-// async function createAssistant(){
-//     const myAssistant = await openai.beta.assistants.create({
-//         instructions: "You are great at recommending movies. When asked a question, use the information in the provided file to form a friendly response. If you cannot find the answer in the file, do your best to infer what the answer should be.",
-//         name: "Movie Buff",
-//         tools: [{type: "file_search"}],
-//         model: "gpt-4-1106-preview",
-//         // file_ids: ["file-YwSkIxszU12VEtI6XWH47T4E"]
-//     });
-//       console.log(myAssistant);
-// }
-
-// createAssistant();
-
-// Create a thread
-const thread = await openai.beta.threads.create();
-console.log(thread)
-
+// Create a message for the thread
+async function createMessage(){
+    const threadMessages = await openai.beta.threads.messages.create(
+    threadID,
+    { role: "user", content: "Can you recommend a comedy?" }
+  );
+  console.log(threadMessages);
+}
+createMessage();
