8a034ab8d20c9e1eabb9364f9681392bfdc644a8
pixelboogie <41452340+pixelboogie@users.noreply.github.com>
2024-07-03 14:51:20 -0500

    10

--- index.js
+++ index.js
@@ -16,12 +16,34 @@ form.addEventListener('submit', function(e) {
 // Bring it all together
 async function main() {
   reply.innerHTML = 'Thinking...';
+  
     await createMessage(input.value);
     
     // create a run
     const run = await runThread();
-    console.log(run);
+   // console.log(run);
+   
+     // Retrieve the current run
+  let currentRun = await retrieveRun(threadID, run.id);
+  
+    // Keep Run status up to date
+  // Poll for updates and check if run status is completed    
+  while (currentRun.status !== 'completed') {
+    await new Promise(resolve => setTimeout(resolve, 1500));
+    console.log(currentRun.status);
+    currentRun = await retrieveRun(threadID, run.id);
+  }
+  
+  
+  // Get messages from the thread
+  const { data } = await listMessages();
+
+  // Display the last message for the current run
+  reply.innerHTML = data[0].content[0].text.value;
+  
 }
+
+
 
 /* -- Assistants API Functions -- */
 
@@ -36,7 +58,11 @@ async function createMessage(question) {
 // Run the assistant's thread
 async function runThread(){
     const run = await openai.beta.threads.runs.create(
-    threadID, { assistant_id: asstID }
+    threadID, 
+    { 
+        assistant_id: asstID,
+         instructions: `Please do not provide annotations in your reply. Only reply about movies in the provided file. If questions are not related to movies, respond with "Sorry, I don't know." Keep your answers short. ` 
+    }
   );
   // console.log(run);
   return run;
