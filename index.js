4499ac70278ccc35472836828d85435f890ec5c7
pixelboogie <41452340+pixelboogie@users.noreply.github.com>
2024-07-03 14:46:31 -0500

    03

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 import { openai } from './config.js';
 
 // Upload a file with an "assistants" purpose
-const file = await openai.files.create({
-  file: await fetch("movies.txt"),
-  purpose: "assistants",
-});
-console.log(file);
+// const file = await openai.files.create({
+//   file: await fetch("movies.txt"),
+//   purpose: "assistants",
+// });
+// console.log(file);
+
+const asstID = "asst_wUu8fCNZ8zvS2UB5OUP4M2ac";
 
 // Create Movie expert assistant
 async function createAssistant(){
     const myAssistant = await openai.beta.assistants.create({
         instructions: "You are great at recommending movies. When asked a question, use the information in the provided file to form a friendly response. If you cannot find the answer in the file, do your best to infer what the answer should be.",
         name: "Movie Buff",
-        tools: [{type: "retrieval"}],
+        tools: [{type: "file_search"}],
         model: "gpt-4-1106-preview",
+        // file_ids: ["file-YwSkIxszU12VEtI6XWH47T4E"]
     });
       console.log(myAssistant);
 }
 
+createAssistant();
